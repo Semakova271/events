28567fedb81d3ed85208917fb746eb93b97a2d7b
Irina Semakova <semakovais@yandex.ru>
2025-07-06 16:05:30 +0300

    Fix GitHub Actions workflow

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,67 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production', // Для деплоя лучше использовать production
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     clean: true,
+    publicPath: '/', // Критически важно для GitHub Pages!
+    assetModuleFilename: 'assets/[hash][ext][query]' // Упорядочиваем ассеты
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader' // Добавляем postcss для автоматических префиксов
+        ]
       },
       {
-        test: /\.(png|svg)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },
-    ],
+        generator: {
+          filename: 'images/[hash][ext][query]' // Все изображения в папку images
+        }
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      minify: true // Минифицируем HTML для production
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css' // Добавляем хэш для кэширования
+    }),
     new ESLintPlugin({
-  overrideConfigFile: path.resolve(__dirname, '.eslintrc.js'),
-  extensions: ['js'],
-  fix: true,
-  emitWarning: true,
-  failOnError: false
-    }),
+      overrideConfigFile: path.resolve(__dirname, '.eslintrc.js'),
+      extensions: ['js'],
+      fix: true,
+      emitWarning: true,
+      failOnError: false
+    })
   ],
   devServer: {
-    static: './dist',
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     hot: true,
-  },
+    port: 3000,
+    open: true,
+    historyApiFallback: true // Важно для SPA на GitHub Pages
+  }
 };
