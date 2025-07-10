8efa2ea9c9e09424c1f1eb2e667d2df32f46330f
Irina Semakova <semakovais@yandex.ru>
2025-07-10 21:30:59 +0300

    6 commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,10 @@
 // eslint.config.js
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+
+const compat = new FlatCompat();
+
 export default [
   {
     ignores: [
@@ -8,10 +14,26 @@ export default [
       'postcss.config.js'
     ]
   },
+  js.configs.recommended,
+  ...compat.extends('plugin:import/recommended'),
   {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        process: 'readonly',
+        console: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly'
+      }
+    },
     rules: {
       'no-console': 'warn',
       'no-unused-vars': 'warn',
+      'no-undef': 'off',
       'quotes': ['error', 'single'],
       'semi': ['error', 'always'],
       'indent': ['error', 2]
