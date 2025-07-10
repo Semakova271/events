87f875d585282631012c25ec300ba8b0644e5262
Irina Semakova <semakovais@yandex.ru>
2025-07-10 21:17:37 +0300

    5 commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,4 @@
-import js from '@eslint/js';
-import globals from 'globals';
-
+// eslint.config.js
 export default [
   {
     ignores: [
@@ -10,28 +8,10 @@ export default [
       'postcss.config.js'
     ]
   },
-  js.configs.recommended,
   {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        process: 'readonly',
-        console: 'readonly',
-        setInterval: 'readonly',
-        clearInterval: 'readonly',
-        require: 'readonly',
-        module: 'readonly',
-        __dirname: 'readonly'
-      }
-    },
     rules: {
       'no-console': 'warn',
       'no-unused-vars': 'warn',
-      'no-undef': 'off',
       'quotes': ['error', 'single'],
       'semi': ['error', 'always'],
       'indent': ['error', 2]
